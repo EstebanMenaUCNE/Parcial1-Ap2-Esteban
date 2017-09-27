fb7de900659e5190b9b20700dacfb084ee233fb3
Esteban Stalin Mena Baldera <esteban_mena@ucne.edu.do>
2017-09-26 23:37:29 -0400

    Creación de la solución y la estructura del proyecto y creación de la Entidad Presupuesto

---
+++ Entidades/Presupuesto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Presupuesto
+    {
+        [Key]
+        public int PresupuestoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public double Monto { get; set; }
+
+        public Presupuesto()
+        {
+
+        }
+
+        public Presupuesto(int presupuestoId, DateTime fecha, string descripcion, double monto)
+        {
+            PresupuestoId = presupuestoId;
+            Fecha = fecha;
+            Descripcion = descripcion;
+            Monto = monto;
+        }
+
+    }
+}
