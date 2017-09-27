08dcffaabf27c69421550e31af8b24d560496743
Esteban Stalin Mena Baldera <esteban_mena@ucne.edu.do>
2017-09-27 00:47:34 -0400

    Clases de la DAL y BLL listas

---
+++ BLL/PresupuestoBLL.cs
@@ -0,0 +1,53 @@
+﻿using DAL;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class PresupuestoBLL
+    {
+        public static bool Guardar(Presupuesto presupuesto)
+        {
+            using (var repositorio = new Repositorio<Presupuesto>())
+            {
+                if (repositorio.Buscar(P => P.PresupuestoId == presupuesto.PresupuestoId) == null)
+                {
+                    return repositorio.Guardar(presupuesto);
+                }
+                else
+                {
+                    return repositorio.Modificar(presupuesto);
+                }
+            }
+        }
+
+        public static Presupuesto Buscar(Expression<Func<Presupuesto, bool>> criterioBusqueda)
+        {
+            using (var repositorio = new Repositorio<Presupuesto>())
+            {
+                return repositorio.Buscar(criterioBusqueda);
+            }
+        }
+
+        public static bool Eliminar(Presupuesto presupuesto)
+        {
+            using (var repositorio = new Repositorio<Presupuesto>())
+            {
+                return repositorio.Eliminar(presupuesto);
+            }
+        }
+
+        public static List<Presupuesto> GetList(Expression<Func<Presupuesto, bool>> criterioBusqueda)
+        {
+            using (var repositorio = new Repositorio<Presupuesto>())
+            {
+                return repositorio.GetList(criterioBusqueda);
+            }
+        }
+    }
+}
---
+++ DAL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        bool Guardar(TEntity entidad);
+        bool Modificar(TEntity entidad);
+        bool Eliminar(TEntity entidad);
+        TEntity Buscar(Expression<Func<TEntity, bool>> criterioBusqueda);
+        List<TEntity> GetList(Expression<Func<TEntity, bool>> criterioBusqueda);
+    }
+}
